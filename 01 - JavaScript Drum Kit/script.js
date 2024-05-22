99be7e19727153b370046113347c68223ff46533
ivanbonnot <ivanbonnot@gmail.com>
2024-05-21 21:38:42 -0300

    listo el 01

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -2,12 +2,23 @@ document.addEventListener('keydown', playSound)
 
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    console.log(audio)
-    if (audio) {
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
 
+    if (audio) {
         audio.currentTime = 0;
         audio.play()
+        key.classList.remove('active');
+        key.classList.add('active')
     } else {
         return
     }
+
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key => key.addEventListener('transitionend', removeActiveClass));
+
+    function removeActiveClass(e) {
+        if (e.propertyName !== 'transform') return;
+        this.classList.remove('active');
+    }
+
 }
